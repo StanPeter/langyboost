b57c157a8026af576ea6fdf56c8305985af1331a
StanPeter <stanislav.petr7@gmail.com>
2021-05-07 00:37:50 +0200

    connecting login page with BE and fetching data on home page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import { useRegisterMutation } from "generated/graphql";
+import { RouteComponentProps } from "react-router";
 
-interface RegisterProps {}
+const Register: React.FC<RouteComponentProps> = ({ history }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [register] = useRegisterMutation();
 
-const Register: React.FC<RegisterProps> = ({}) => {
-    return <div>Register page</div>;
+    const onSubmitHandler = async (e: FormEvent) => {
+        e.preventDefault();
+
+        const response = await register({ variables: { email, password } });
+        console.log(response, "response");
+        history.push("/");
+    };
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div>
+                <input
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="text"
+                />
+            </div>
+            <div>
+                <input
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default Register;
