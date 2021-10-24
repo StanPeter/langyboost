c87d30d9291b311b2b17c8617d6969649adc1bf3
Stanislav <stanislav.petr7@gmail.com>
2021-10-24 19:35:24 +0200

    Added design images

---
+++ client/src/pages/TermsConditionsPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface TermsConditionsPageProps {}
+
+const TermsConditionsPage: React.FC<TermsConditionsPageProps> = ({}) => {
+    return <div>{}</div>;
+};
+
+export default TermsConditionsPage;
