5a6d5d8f912247d19f2fdac38e792ff71894cb5b
Stanislav Petr <stanislav.petr7@gmail.com>
2025-02-10 21:03:09 +0100

    migrate to swiper

--- client/src/components/others/LoginForm/Slider.tsx
+++ client/src/components/others/LoginForm/Slider.tsx
@@ -1,88 +1,162 @@
-import Header from 'components/UI/Header/Header';
+import { Box, styled } from '@mui/material';
+import Header from 'components/UI/Header';
 import { LOGIN_FORM_TEXT } from 'constants/constants';
-import React, { useState } from 'react';
-import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
-import { useInterval } from 'usehooks-ts';
+import React from 'react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-interface ISliderProps {}
+// Import required modules
 
-// might be used for other usecases in future
-const Slider: React.FC<ISliderProps> = ({}) => {
-    const [slideInfo, setSlideInfo] = useState<{ index: number; direction: 'none' | 'right' | 'left' }>({
-        index: 2,
-        direction: 'none',
-    });
-    const [lastClickTime, setLastClickTime] = useState(new Date());
+// const StyledIconFiArrowLeft = styled(FiArrowLeft)`
+//     width: 20px;
+//     height: 20px;
+// `;
 
-    // automate aniamtion
-    const autoAnimationHandler = () => {
-        const currentTime = new Date();
-        if (Math.floor(currentTime.getSeconds() - lastClickTime.getSeconds()) > 5) {
-            slideRightHandler();
-        }
-    };
+// const StyledIconFiArrowRight = styled(FiArrowRight)`
+//     width: 20px;
+//     height: 20px;
+// `;
 
-    // set interval to check each second
-    useInterval(autoAnimationHandler, 1000);
+// const StyledArrowBox = styled(Box)`
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     cursor: pointer;
+// `;
 
-    const slideLeftHandler = () => {
-        setSlideInfo(state => ({
-            direction: 'left',
-            index: state.index === 1 ? LOGIN_FORM_TEXT.length : state.index - 1,
-        }));
-        setLastClickTime(new Date());
-    };
+// const StyledTextWrapper = styled(Box)`
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     position: absolute;
+//     width: 100%;
+// `;
 
-    const slideRightHandler = () => {
-        setSlideInfo(state => ({
-            direction: 'right',
-            index: state.index === LOGIN_FORM_TEXT.length ? 1 : state.index + 1,
-        }));
+// const StyledWrapper = styled(Box)`
+//     display: flex;
+//     flex-direction: row;
+//     width: 100%;
+//     justify-content: space-between;
+//     align-items: center;
+//     background-color: var(--color-main);
+//     color: var(--color-text-light);
+//     height: 10rem; // Converted 40 to rem for consistency
+//     border-top-left-radius: 1.5rem; // Adjusted to match your theme
+//     border-top-right-radius: 1.5rem;
+// `;
 
-        setLastClickTime(new Date());
-    };
+// const StyledBox = styled(Box)`
+//     display: flex;
+//     flex-direction: row;
+//     width: 100%;
+//     justify-content: space-between;
+//     align-items: center;
+// `;
 
-    const animationHandler = (indexOfHeader: number) => {
-        // left direction flow of animation
-        if (slideInfo.direction === 'left') {
-            if (indexOfHeader + 1 === slideInfo.index) return 'slideRightToZero';
-            else if (indexOfHeader + 1 === slideInfo.index + 1) return 'slideZeroToLeft';
-        }
-        // right direction flow of animation
+// interface ISliderProps {}
 
-        if (slideInfo.direction === 'right') {
-            if (indexOfHeader + 1 === slideInfo.index) return 'slideLeftToZero';
-            else if (indexOfHeader + 1 === slideInfo.index - 1) return 'slideZeroToRight';
-        }
+// // might be used for other usecases in future
 
+// const Slider: React.FC<ISliderProps> = ({}) => {
+//     const [slideInfo, setSlideInfo] = useState<{ index: number; direction: 'none' | 'right' | 'left' }>({
+//         index: 2,
+//         direction: 'none',
+//     });
+//     const [lastClickTime, setLastClickTime] = useState(new Date());
 
-        // initial state for animation
-        if (slideInfo.direction === 'none' && indexOfHeader + 1 === 2) return '';
+//     // automate aniamtion
+//     const autoAnimationHandler = () => {
+//         const currentTime = new Date();
+//         if (Math.floor(currentTime.getSeconds() - lastClickTime.getSeconds()) > 5) {
+//             slideRightHandler();
+//         }
+//     };
 
-        // do not display elements other than the 2 animated
-        return 'hidden';
-    };
+//     // set interval to check each second
+//     useInterval(autoAnimationHandler, 1000);
 
+//     const slideLeftHandler = () => {
+//         setSlideInfo(state => ({
+//             direction: 'left',
+//             index: state.index === 1 ? LOGIN_FORM_TEXT.length : state.index - 1,
+//         }));
+//         setLastClickTime(new Date());
+//     };
+
+//     const slideRightHandler = () => {
+//         setSlideInfo(state => ({
+//             direction: 'right',
+//             index: state.index === LOGIN_FORM_TEXT.length ? 1 : state.index + 1,
+//         }));
+
+//         setLastClickTime(new Date());
+//     };
+
+//     const animationHandler = (indexOfHeader: number) => {
+//         // left direction flow of animation
+//         if (slideInfo.direction === 'left') {
+//             if (indexOfHeader + 1 === slideInfo.index) return 'slideRightToZero';
+//             else if (indexOfHeader + 1 === slideInfo.index + 1) return 'slideZeroToLeft';
+//         }
+//         // right direction flow of animation
+
+//         if (slideInfo.direction === 'right') {
+//             if (indexOfHeader + 1 === slideInfo.index) return 'slideLeftToZero';
+//             else if (indexOfHeader + 1 === slideInfo.index - 1) return 'slideZeroToRight';
+//         }
+
+//         // initial state for animation
+//         if (slideInfo.direction === 'none' && indexOfHeader + 1 === 2) return '';
+
+//         // do not display elements other than the 2 animated
+//         return 'hidden';
+//     };
+
+//     return (
+//         <StyledWrapper>
+//             <StyledArrowBox onClick={slideLeftHandler}>
+//                 <StyledIconFiArrowLeft />
+//             </StyledArrowBox>
+//             <StyledBox>
+//                 {LOGIN_FORM_TEXT.map((slideText, i: number) => (
+//                     <StyledTextWrapper key={slideText} className={`${animationHandler(i)}`}>
+//                         <Header level={2} whiteText text={slideText} />
+//                     </StyledTextWrapper>
+//                 ))}
+//             </StyledBox>
+//             <StyledArrowBox onClick={slideRightHandler}>
+//                 <StyledIconFiArrowRight />
+//             </StyledArrowBox>
+//         </StyledWrapper>
+//     );
+// };
+
+const StyledWrapper = styled(Box)`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--color-main);
+    color: var(--color-text-light);
+    height: 10rem;
+    border-top-left-radius: 1.5rem;
+    border-top-right-radius: 1.5rem;
+`;
+
+const Slider: React.FC = () => {
     return (
-        <div
-            className={
-                'flex flex-row w-full justify-between items-center bg-[var(--color-main)] text-[var(--color-text-light)] h-40 rounded-t-3xl'
-            }
-        >
-            <div onClick={slideLeftHandler} className="h-full items-center flex pr-10 cursor-pointer hover:scale-110">
-                <FiArrowLeft className={'w-8 h-8'} />
-            </div>
-            <div className={`flex-1 justify-center relative items-center flex`}>
-                {LOGIN_FORM_TEXT.map((slideText, i: number) => (
-                    <div key={slideText} className={`text-center absolute w-full  ${animationHandler(i)}`}>
+        <StyledWrapper>
+            <Swiper modules={[Navigation]} navigation spaceBetween={50} slidesPerView={1}>
+                {LOGIN_FORM_TEXT.map((slideText, i) => (
+                    <SwiperSlide key={i}>
                         <Header level={2} whiteText text={slideText} />
-                    </div>
+                    </SwiperSlide>
                 ))}
-            </div>
-            <div onClick={slideRightHandler} className="h-full items-center flex pl-10 cursor-pointer hover:scale-110">
-                <FiArrowRight className={'w-8 h-8'} />
-            </div>
-        </div>
+            </Swiper>
+        </StyledWrapper>
     );
 };
 
