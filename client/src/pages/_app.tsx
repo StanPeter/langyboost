7c68fb63966e96823c24faffdfc3decdfbb44246
Stanislav Petr <stanislav.petr7@gmail.com>
2025-02-11 15:36:22 +0100

    adding next auth

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import { ThemeProvider } from '@mui/material/styles';
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import 'styles/globalClasses.css';
 import 'styles/globalStyles.css';
 import { theme } from 'styles/theme';
 
-
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
-        <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <SessionProvider session={pageProps.session}>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </SessionProvider>
     );
-}
-
+};
 
 export default MyApp;
