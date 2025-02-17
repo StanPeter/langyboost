71e3b56e0a94bf364a823366b1a8f366666a3521
Stanislav Petr <stanislav.petr7@gmail.com>
2025-02-17 22:35:03 +0100

    cleaning up cards

--- client/src/components/others/PhraseCardsControls/phraseCardsControls.module.scss.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const controlContinueDown: string;
-export declare const controlContinueUp: string;
-export declare const controlIcon: string;
-export declare const controlThumbsDown: string;
-export declare const controlThumbsUp: string;
