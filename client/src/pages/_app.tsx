a2639fa7c894894f3ae7dbbd4233d02c8ffa9290
Stanislav Petr <stanislav.petr7@gmail.com>
2025-02-20 16:50:54 +0100

    fixing react tanstack error of not found

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { ThemeProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
 import ErrorFallback from 'components/hoc/ErrorBoundaryHoc';
 import { logErrorFromClient } from 'lib/errorLogger';
+import { trpc } from 'lib/trpcClient';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Provider } from 'react-redux';
 import store from 'store';
@@ -11,16 +15,31 @@ import 'styles/globalStyles.css';
 import { theme } from 'styles/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: '/api/trpc',
+                }),
+            ],
+        }),
+    );
+
     return (
-        <ErrorBoundary FallbackComponent={ErrorFallback} onError={logErrorFromClient}>
-            <Provider store={store}>
-                <SessionProvider session={pageProps.session}>
-                    <ThemeProvider theme={theme}>
-                        <Component {...pageProps} />
-                    </ThemeProvider>
-                </SessionProvider>
-            </Provider>
-        </ErrorBoundary>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                <ErrorBoundary FallbackComponent={ErrorFallback} onError={logErrorFromClient}>
+                    <Provider store={store}>
+                        <SessionProvider session={pageProps.session}>
+                            <ThemeProvider theme={theme}>
+                                <Component {...pageProps} />
+                            </ThemeProvider>
+                        </SessionProvider>
+                    </Provider>
+                </ErrorBoundary>
+            </QueryClientProvider>
+        </trpc.Provider>
     );
 };
 
